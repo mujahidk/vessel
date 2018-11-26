628462f2c98777116b49210b845b062bae0b5937
Mujahid Khaleel <mkhaleel@virmedica.com>
2018-11-26 12:45:00 -0500

    Add new Chameleopn application.

---
+++ chameleon/src/test/java/com/mujahidk/vessel/chameleon/ChameleonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mujahidk.vessel.chameleon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChameleonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
