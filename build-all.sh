f0c5d678befe152481a8b5f573ac8b21bded0e97
Mujahid Khaleel <mkhaleel@virmedica.com>
2019-01-24 13:30:47 -0500

    Update build and docker references.

--- build-all.sh
+++ build-all.sh
@@ -4,6 +4,10 @@ echo Building question service.
 cd question-service/
 ./mvnw clean package
 
+echo Building titan application.
+cd ../titan
+./build
+
 cd ..
 echo Starting Docker compose
 docker-compose up -d --build
